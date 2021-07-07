20274e0efc66af89678df7f4e2d43251f8f9d045
Nick Cardin <nick@cardin.email>
2021-07-07 03:54:26 +0000

    update purge opts

--- rollup-plugin-smelte.js
+++ rollup-plugin-smelte.js
@@ -2,22 +2,24 @@ const postcss = require("rollup-plugin-postcss");
 const path = require("path");
 const extractor = require("./src/utils/css-extractor.js");
 
-const defaultWhitelist = ["html", "body", "stroke-primary", "mode-dark"];
-
-const defaultWhitelistPatterns = [
+const defaultWhitelist = [
+  "html",
+  "body",
+  "stroke-primary",
+  "mode-dark",
   // for JS ripple
   /ripple/,
   // date picker
-  /w\-.\/7/
+  /w\-.\/7/,
 ];
 
 const postcssProcessor = ({
   tailwind = {},
   postcss = [],
-  whitelist = defaultWhitelist,
-  whitelistPatterns = defaultWhitelistPatterns,
-  whitelistPatternsChildren = defaultWhitelistPatterns,
-  purge = false
+  safelist = [],
+  blocklist = [],
+  skippedContentGlobs = [],
+  purge = false,
 }) => {
   const tailwindConfig = require("./tailwind.config.js")(tailwind);
   return [
@@ -29,7 +31,7 @@ const postcssProcessor = ({
     ...postcss,
     purge &&
       require("cssnano")({
-        preset: "default"
+        preset: "default",
       }),
     purge &&
       require("@fullhuman/postcss-purgecss")({
@@ -37,21 +39,23 @@ const postcssProcessor = ({
         extractors: [
           {
             extractor,
-            extensions: ["svelte"]
-          }
+            extensions: ["svelte"],
+          },
         ],
-        whitelist: whitelist.filter(Boolean),
-        whitelistPatterns: whitelistPatterns.filter(Boolean),
-        whitelistPatternsChildren: whitelistPatternsChildren.filter(Boolean)
-      })
+        safelist: defaultWhitelist.concat(safelist.filter(Boolean)),
+        blocklist: blocklist.filter(Boolean),
+        skippedContentGlobs: skippedContentGlobs.filter(Boolean),
+      }),
   ].filter(Boolean);
 };
 
 function moveGlobalCssToStatic({ dir, file }, output) {
   let directory;
-  if (dir && !file) { // single file asset
+  if (dir && !file) {
+    // single file asset
     directory = dir;
-  } else if (!dir && file) { // chunks
+  } else if (!dir && file) {
+    // chunks
     directory = path.dirname(file);
   }
 
@@ -59,43 +63,42 @@ function moveGlobalCssToStatic({ dir, file }, output) {
     return;
   }
 
-  const fs = require('fs')
-  const cssFile = path.basename(output)
+  const fs = require("fs");
+  const cssFile = path.basename(output);
   if (fs.existsSync(path.resolve(directory, cssFile))) {
-    fs.copyFileSync(path.resolve(directory, cssFile), path.resolve(output))
-    fs.unlinkSync(path.resolve(directory, cssFile))
-  
+    fs.copyFileSync(path.resolve(directory, cssFile), path.resolve(output));
+    fs.unlinkSync(path.resolve(directory, cssFile));
+
     console.log(
       `rollup-plugin-smelte: 
         moved ${cssFile} 
         from ${path.resolve(directory, cssFile)} 
         to ${path.resolve(output)}`
-    )
+    );
   }
 }
 
-const plugins = config => postcssProcessor(config || {});
+const plugins = (config) => postcssProcessor(config || {});
 
-module.exports = (config = {}) =>
-{
-  const defaultOutput = "./static/global.css"
+module.exports = (config = {}) => {
+  const defaultOutput = "./static/global.css";
   const pcss = postcss({
     plugins: plugins(config),
-    extract: path.basename(config.output || defaultOutput)
+    extract: path.basename(config.output || defaultOutput),
   });
 
   // since rollup v2 we can't extract css outside of the defined output, so to keep the impact to this
   // plugin minimal I injected some code to the "writeBundle" method (which should be called after "generateBundle")
   // to move the global.css file located under rollup.config.js's rollup.output to config parameter's output
 
-  const old_writeBundle = pcss.writeBundle
+  const old_writeBundle = pcss.writeBundle;
   pcss.writeBundle = function() {
     if (old_writeBundle) {
-      old_writeBundle.apply(this, arguments)
+      old_writeBundle.apply(this, arguments);
     }
     // arguments[0]: OutputOptions
-    moveGlobalCssToStatic(arguments[0], config.output || defaultOutput)
-  }
+    moveGlobalCssToStatic(arguments[0], config.output || defaultOutput);
+  };
 
   return pcss;
-}
+};
